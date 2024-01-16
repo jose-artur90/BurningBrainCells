7f318c204627cb166007c39a24c20d66ee54ca1d
jose-artur90 <62076500+jose-artur90@users.noreply.github.com>
2024-01-16 21:08:34 +0000

    atualização

---
+++ Python/Práticas de Python/Capitulo1/1.10.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    E1, E2, E3, E4, E5= 4==5, 4!=6, 4<5 and 6>10, 40<50 or 60>90 , not(40>5 or 60<150)
+    print(f"{E1}")
+    print(f"{E2}")
+    print(f"{E3}")
+    print(f"{E4}")
+    print(f"{E5}")
+    
+    
---
+++ Python/Práticas de Python/Capitulo1/1.11.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    E1, E2, E3, E4, E5= math.pow(2,3), math.sqrt(125), math.floor(-4.6), math.log10(1300), math.sin(60)
+    print(f"{E1:0.0f}")
+    print(f"{E2:0.4f}")
+    print(f"{E3:0.0f}")
+    print(f"{E4:0.8f}")
+    print(f"{E5:0.8f}")
+    
+    
